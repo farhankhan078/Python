d58b6b0b4e54d6977f52cfc2b00979638ddc956a
--edit <farhanknawab@gmail.com>
2024-08-21 19:23:09 +0530

    2nd commit

---
+++ find_name.py
@@ -0,0 +1,7 @@
+a = ["Aman","Sonakshi","Sunaina","Sumitra","Daisy","Muskan","Amir","Akash","Aliza"]
+name = input("Enter name to check: ")
+
+if(name in a):
+    print("Yay! Your name is in the list")
+else:
+    print("oops Your name is not in the list")
