9b72cf703ece1a12519ff51c5263a60ef8f8a722
--edit <farhanknawab@gmail.com>
2024-08-25 21:14:08 +0530

    Random ideas using python

---
+++ prmpt.py
@@ -0,0 +1,9 @@
+import random
+idea = ["Give them treat",
+         "Get them to movie",
+         "Give him money",
+         "Baking or cooking",
+         "Night Dinner",
+         "Nightout"]
+random_idea = random.choice(idea)
+print("Random idea is: ",random_idea)
