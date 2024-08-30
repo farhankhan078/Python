a99889e7007b07f2b35b6826fa8699cdaa4ee22b
--edit <farhanknawab@gmail.com>
2024-08-30 19:31:52 +0530

    Down Payment calculator

---
+++ house_down_payment_calculator.py
@@ -0,0 +1,12 @@
+price_of_house = 1000000
+cr_score = int(input("Enter credit score: "))
+
+if (cr_score < 300 or cr_score > 850):
+    print("Invalid credit score. Please enter a score between 300 and 850.")
+else:
+    
+    if (cr_score >=740):
+        down_pymnt = (price_of_house * 0.10)
+    else:
+        down_pymnt = (price_of_house * 0.20)
+    print(f"They need to pay ${down_pymnt} to book house")
