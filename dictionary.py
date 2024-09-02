56dac931752cbb556814547af5e7f4c0a7f48590
--edit <farhanknawab@gmail.com>
2024-09-02 23:11:40 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+a = {}
+words = input("Input name: ")
+language = input("Input Language: ")
+a.update({words:language})
+
+words = input("Input name: ")
+language = input("Input Language: ")
+a.update({words:language})
+
+words = input("Input name: ")
+language = input("Input Language: ")
+a.update({words:language})
+
+words = input("Input name: ")
+language = input("Input Language: ")
+a.update({words:language})
+
+print("Uniques names are: ",a)
